207407be735aa41df082cd457e5e1c4a89468e61
Nassahtech <nassahtechkeys@gamil.com>
2022-10-18 20:51:14 -0700

    10 functions

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * addd - add two numbers
+ * @a: the first number
+ * @b: the second number
+ * Return: the additio of both
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
