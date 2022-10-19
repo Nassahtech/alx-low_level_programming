a3c41d508b2228a9c2f0659ef11815bc34d4955e
Nassahtech <nassahtechkeys@gamil.com>
2022-10-18 20:18:30 -0700

    7 last digit function

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,18 +3,18 @@
 /**
  * print_last_digit - prints the last digit
  * @n: the number
- * Return: value of last digit
+ * Return: value of the last digit
  */
 int print_last_digit(int n)
 {
 	int ld = n % 10;
 
 	if (ld < 0)
-	        ld *= -1;
+		ld *= -1;
 
 	_putchar(ld + '0');
-	
-	return (0);
-}
 
+	return (0);
+
+}
 
